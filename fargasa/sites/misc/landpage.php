6d31da6521c66af63fc4e4d82182d77cb3c29c1b
ikakkura <62897994+ikakkura@users.noreply.github.com>
2021-05-05 21:41:33 +0700

    Update landpage.php
    penambahan profil dan kontak

--- fargasa/sites/misc/landpage.php
+++ fargasa/sites/misc/landpage.php
@@ -15,6 +15,8 @@ $data = mysqli_query($con, " SELECT * FROM stok");
   <link rel="icon" type="image/png" href="/fargasa/assets/Fargasa Logo Circle.png" />
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="/fargasa/dist/css/bootstrap.css">
+  <link rel="stylesheet" href="/fargasa/dist/css/style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
 </head>
 
 <body>
@@ -61,17 +63,17 @@ $data = mysqli_query($con, " SELECT * FROM stok");
       </ul>
     </div>
   </nav>
-
+  <!--   carousel   -->
   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img class="d-block w-100" src="../public/img/aven.jpg" alt="First slide" style="height: 480px">
+        <img class="d-block w-100 img-fluid" src="/fargasa/assets/imk/fargasa1.jpg" alt="First slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100" src="../public/img/aven.jpg" alt="Second slide" style="height: 480px">
+        <img class="d-block w-100" src="/fargasa/assets/imk/bg.jpeg" alt="Second slide">
       </div>
       <div class="carousel-item">
-        <img class="d-block w-100" src="../public/img/aven.jpg" alt="Third slide" style="height: 480px">
+        <img class="d-block w-100" src="/fargasa/assets/imk/bg.jpeg" alt="Third slide">
       </div>
     </div>
     <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -85,15 +87,15 @@ $data = mysqli_query($con, " SELECT * FROM stok");
   </div>
   </div>
 
-
-  <div class="container-fluid">
+  <!--   catalog   -->
+  <div class="container-fluid" style="margin-top:50px;">
     <a name="catalog">
-      <h4 class="text-center font-weight-bold">Gallery Mobil</h4>
+      <h4 class="text-center " style="font-size:40px; color:gray; font-family: Glegoo,serif;">Gallery Mobil</h4>
       <div class="container-fluid mt-4 row d-flex justify-content-center">
         <?php foreach ($data as $elements) : ?>
           <div class="row d-inline-flex mx-2">
             <div class="card m-2 " style="width: 18rem;">
-              <img src="..." class="card-img-top" alt="...">
+              <img class="img-fluid" src="<?= $elements["gambar"] ?>" class="card-img-top" alt="...">
               <div class="card-body">
                 <p class="card-text d-none" id="id"><b><?= $elements["id_pembelian"] ?></b></p>
                 <h5 class="card-title" id="tipe"><b><?= $elements["tipe"] ?></b></h5>
@@ -125,12 +127,68 @@ $data = mysqli_query($con, " SELECT * FROM stok");
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary">Save changes</button>
+          <button type="button" class="btn btn-primary">Booking Now</button>
         </div>
       </div>
     </div>
   </div>
 
+  <!--Profil  -->
+  <div class="container mt-5 profil" style=" max-width: 100%;">
+    <a name="profil">
+      <h4 style="text-align: center; font-family: Glegoo,serif; font-size:40px; color:gray;">Why Choose Us</h4>
+      <div class="container-fluid text-center">
+        <p>CV. Fargasa Pratama Raya is committed to helping its clients reach their own cars </p>
+        <p>We offer quality products that are cheaper than our competitors</p>
+      </div>
+      <div class="container-fluid d-flex justify-content-center ">
+        <div class=" text-center ">
+          <img class=" img-fluid" src="/fargasa/assets/imk/profil-img/respect-clients.png" alt="">
+          <p>Respect of their Client needs</p>
+        </div>
+        <div class=" text-center ">
+          <img class="img-fluid" src="/fargasa/assets/imk/profil-img/negotiation-power.png" alt="">
+          <p>Negotiation Able</p>
+        </div>
+        <div class=" text-center ">
+          <img class="img-fluid" src="/fargasa/assets/imk/profil-img/flexibility-efficient-solutions.png" alt="">
+          <p>Effiecien Work</p>
+        </div>
+        <div class=" text-center ">
+          <img class="img-fluid" src="/fargasa/assets/imk/profil-img/focus-on-innovation.png" alt="">
+          <p>Focus on Client Need</p>
+        </div>
+        <div class=" text-center ">
+          <img class="img-fluid" src="/fargasa/assets/imk/profil-img/global-know-how.png" alt="">
+          <p>Huge Connection</p>
+        </div>
+      </div>
+  </div>
+
+  <!-- Contact -->
+  <div class="container py-5 " style=" max-width: 100%;">
+    <a name="contact">
+      <h4 style="text-align: center; font-family: Glegoo,serif; font-size:40px">CONTACT US AT</h4>
+      <div class="container  w-50">
+        <div class="row " style="text-align: center; ">
+          <div class="col my-1 mx-1 pt-3 " style="background-color: green; border-radius: 20px;  min-width: 100px;">
+            <a href="" class="fa fa-whatsapp" aria-hidden="true" style="text-decoration: none; font-size: 30px; color:white;">Alam </a>
+            <p style="color:white; font-size: 1vw;">Senior Sales Fargasa</p>
+          </div>
+          <div class="col my-1 mx-1 pt-3" style="background-color: green; border-radius: 20px;  min-width: 100px;">
+            <a href="" class="fa fa-whatsapp" aria-hidden="true" style="text-decoration: none; font-size: 30px; color:white;">Ninit</a>
+            <p style="color:white; font-size: 1vw;">CEO Fargasa</p>
+          </div>
+        </div>
+
+      </div>
+  </div>
+
+  <footer style="background-color: black;">
+    <div class="footer-copyright text-center py-3">© 2021 Copyright:
+      <a href=""> Alibaba Group</a>
+    </div>
+  </footer>
   <script src="/fargasa/dist/js/jquery-3.5.1.js"></script>
   <script src="/fargasa/dist/js/bootstrap.js"></script>
 </body>
